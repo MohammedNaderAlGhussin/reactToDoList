f27ac8067de2d46570a38ceb6c65a3adf65bab52
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-10-23 21:50:39 +0300

    handling the active button and updatign the filter state

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TasksContext } from "../context/TasksContext";
 
 const NavButton = () => {
+  const { filter, setFilter } = useContext(TasksContext);
   return (
     <ul className="flex flex-row gap-4 mt-4">
-      <li className="btn active">All</li>
-      <li className="btn">Done</li>
-      <li className="btn">Not Done</li>
+      <li
+        className={`btn ${filter == "all" ? "active" : ""}`}
+        onClick={() => setFilter("all")}
+      >
+        All
+      </li>
+      <li
+        className={`btn ${filter == "done" ? "active" : ""}`}
+        onClick={() => setFilter("done")}
+      >
+        Done
+      </li>
+      <li
+        className={`btn ${filter == "not done" ? "active" : ""}`}
+        onClick={() => setFilter("not done")}
+      >
+        Not Done
+      </li>
     </ul>
   );
 };
